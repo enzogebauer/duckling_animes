3b09f912d10b1607420fe6b1fc8ef36df679afa3
Enzo Gebauer <enzo.gebauer@aln.senaicimatec.edu.br>
2023-05-07 16:04:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/duckling_movies/AnimeManage.java
@@ -0,0 +1,14 @@
+package com.example.duckling_movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimeManage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime_manage);
+    }
+}
---
+++ app/src/main/java/com/example/duckling_movies/PostAnimes.java
@@ -0,0 +1,14 @@
+package com.example.duckling_movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostAnimes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_animes);
+    }
+}
